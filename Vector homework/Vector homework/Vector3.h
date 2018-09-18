4f0ecd6b613b6a186fbcf9eb954cf7aad7dfda5b
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-09-18 12:32:57 +0200

    optimize class code

--- Vector homework/Vector homework/Vector3.h
+++ Vector homework/Vector homework/Vector3.h
@@ -1,23 +1,21 @@
 #ifndef  _VECTOR3_H_
 #define  _VECTOR3_H_
- 
 #include <math.h>
 
 template <class T>
 
-
-
-class vec {
+class vec 
+{
 private:
 	T x, y, z;
 public:
 
 	//constructor
-	vec() : x(0), y(0), z(0) {}
+	vec() {}
 
-	//falta uno
+	vec(const vec& vector) : x(vector.x), y(vector.y), z(vector.z) {}
 
-	vec(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+	vec(T x, T y, T z) : x(x), y(y), z(z) {}
 
 	//desturctor
 
@@ -25,77 +23,64 @@ public:
 
 	//operators
 
-	vec operator+ (const vec &vector) {
-		vec sumvec;
-		sumvec.x = x + vector.x;
-		sumvec.y = y + vector.y;
-		sumvec.z = z + vector.z;
-		return sumvec;
+	vec operator+ (const vec &vector) const
+	{
+		return vec ((x + vector.x), (y + vector.y), (z + vector.z));
+		
 	}
 
-	vec operator- (const vec &vector) {
-		vec resvec;
-		resvec.x = x - vector.x;
-		resvec.y = y - vector.y;
-		resvec.z = z - vector.z;
-		return resvec;
+	vec operator- (const vec &vector) const
+	{
+		return vec (x - vector.x, y - vector.y, z - vector.z);
 	}
 
-	void operator+= (const vec &vector) {
+	vec operator+= (const vec &vector) 
+	{
 		x += vector.x;
 		y += vector.y;
 		z += vector.z;
+		return *this;
 	}
 
-	void operator-= (const vec &vector) {
+	vec operator-= (const vec &vector) 
+	{
 		x -= vector.x;
 		y -= vector.y;
 		z -= vector.z;
-
+		return *this;
 	}
 
-	bool operator== (const vec &vector) const {
-		if (x == vector.x && y == vector.y && z == vector.z) {
-			return true;
-		}
-		else {
-			return false;
-		}
-
+	bool operator== (const vec &vector) const 
+	{
+		return (x == vector.x && y == vector.y && z == vector.z); 
 	}
 
-
-
-	void operator= (const vec &vector) {
+	vec operator= (  vec &vector) 
+	{
 		x = vector.x;
 		y = vector.y;
 		z = vector.z;
+		return *this;
 	}
 
+	//methods
 
-
-	//
-
-	vec normalize() {
-		vec normievec;
-		T length;
-		length = sqrt((x*x) + (y*y) + (z*z));
-		normievec.x = x / length;
-		normievec.y = y / length;
-		normievec.z = z / length;
-		return normievec;
+	vec normalize() 
+	{
+		T length = sqrt((x*x) + (y*y) + (z*z));
+		return vec (x / length, y / length, z / length);
 	}
 
 
 	bool is_zero( const vec vector) const 
 	{
-		if (vector.x == 0 && vector.y == 0 && vector.z == 0)
-			return true;
-		else
-			return false;
+		return (vector.x == 0 && vector.y == 0 && vector.z == 0);
 	}
 
-	void zero(vec &vector) { vector.x = vector.y = vector.z = 0; }
+	void zero(vec &vector) 
+	{ 
+		vector.x = vector.y = vector.z = 0; 
+	}
 
 	void distance_to( const vec &vector) const
 	{
@@ -104,20 +89,13 @@ public:
 		b = y - vector.y;
 		c = z - vector.z;
 		cout << "the distances from each component of each vector are " << "x: " << a << " y: " << b << " z:  " << c << endl;
-
 	}
 
-	void print() const {
-
+	void print() const 
+	{
 		cout << "x: " << x << " y: " << y << " z: " << z << endl;
 	}
 };
-
-
-
-
-
-
 
 
 #endif // ! _VECTOR3_H_
