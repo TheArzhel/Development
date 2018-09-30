54b63cc8633da3d480b78255555d8cc32333393a
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-10-01 00:02:25 +0200

    todo 3 coompleted

--- Dev_class5_handout/Motor2D/j1Map.h
+++ Dev_class5_handout/Motor2D/j1Map.h
@@ -98,7 +98,7 @@ private:
 	bool LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set);
 	bool LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set);
 	// TODO 3: Create a method that loads a single laye
-	// bool LoadLayer(pugi::xml_node& node, MapLayer* layer);
+	bool LoadLayer(pugi::xml_node& node, MapLayer* layer);
 
 public:
 
